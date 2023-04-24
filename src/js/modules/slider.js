b3438e694722227a35272cffaea72bc637e57e38
viacheslav_demchenko <frontend-ninja@yandex.ru>
2023-04-24 21:08:37 +0300

    made slider

--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -1,84 +1,15 @@
-import Swiper from './../libs/swiper-bundle.min.js'; // Слайдер
+// import Slick from './../libs/slick.js'; // Слайдер
+// import $ from './../libs/jquery.js';
 
 export default function slider() {
-  if(document.querySelector('.thanks__slider')) {
-    const swiper = new Swiper('.thanks__slider', {
-      spaceBetween: 42,
-      loopedSlides: 1,
-      slidesPerView: 3,
-      // centeredSlides: true,
-      // loop: true,
-      // cssMode: true,
-      autoHeight: true,
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      breakpoints: {
-        // 320: {
-        //   spaceBetween: 0,
-        //   loopedSlides: 1,
-        //   slidesPerView: 1,
-        //   autoHeight: true
-        // },
-        // 700: {
-        //   spaceBetween: 20,
-        //   loopedSlides: 1,
-        //   slidesPerView: 2
-        // },
-        // 1024: {
-        //   spaceBetween: 30,
-        //   loopedSlides: 1,
-        //   slidesPerView: 3
-        // },
-        // 1440: {
-        //   spaceBetween: 42,
-        //   loopedSlides: 1,
-        //   slidesPerView: 3
-        // },
-      }
-    });
-  }
-
-  // if(document.querySelector('.thanks__slider-bottom')) {
-  //   const swiper = new Swiper('.thanks__slider-bottom', {
-  //     spaceBetween: 45,
-  //     loopedSlides: 1,
-  //     slidesPerView: 2.1,
-  //     // slidesPerView: "auto",
-  //     // centeredSlides: true,
-  //     // centeredSlides: false,
-  //     // loop: true,
-  //     // autoHeight: true,
-  //     navigation: {
-  //       nextEl: '.swiper-button-next',
-  //       prevEl: '.swiper-button-prev',
-  //     },
-  //     breakpoints: {
-  //       // 320: {
-  //       //   spaceBetween: 0,
-  //       //   loopedSlides: 1,
-  //       //   slidesPerView: 1,
-  //       //   autoHeight: true
-  //       // },
-  //       // 700: {
-  //       //   spaceBetween: 20,
-  //       //   loopedSlides: 1,
-  //       //   slidesPerView: 2
-  //       // },
-  //       // 1024: {
-  //       //   spaceBetween: 30,
-  //       //   loopedSlides: 1,
-  //       //   slidesPerView: 3
-  //       // },
-  //       // 1440: {
-  //       //   spaceBetween: 42,
-  //       //   loopedSlides: 1,
-  //       //   slidesPerView: 3,
-  //       //   slidesPerView: "auto",
-  //       //   centeredSlides: true,
-  //       // },
-  //     }
-  //   });
-  // }
+  $('#slick1').slick({
+    rows: 2,
+    dots: false,
+    arrows: true,
+    infinite: true,
+    adaptiveHeight: true,
+    speed: 300,
+    slidesToShow: 2,
+    slidesToScroll: 2
+  });
 }
